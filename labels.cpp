fbcb5e8341fccf1658627346b95533e73c0b8215
Magnus Andersen <magnuan@gmail.com>
2023-09-20 13:52:05 +0200

    Disable some of the default classes

--- labels.cpp
+++ labels.cpp
@@ -7,7 +7,7 @@ std::vector<Label> getLabels() {
     l.emplace_back("unclassified", 0, 1, Color(255, 255, 255));
 
     l.emplace_back("ground", 1, 2, Color(192, 192, 192));
-   *
+    /*
     l.emplace_back("low_vegetation", 2, 3, Color(0, 128, 0));
     l.emplace_back("medium_vegetation", 3, 4, Color(0, 128, 0));
     l.emplace_back("high_vegetation", 4, 5, Color(0, 128, 0));
@@ -20,7 +20,7 @@ std::vector<Label> getLabels() {
     l.emplace_back("road_surface", 9, 11, Color(110, 110, 110));
     l.emplace_back("wire_guard", 10, 13, Color(135, 135, 135));
     l.emplace_back("wire_conductor", 11, 14, Color(255, 255, 255));
-    
+    */
     l.emplace_back("transmission_tower", 12, 15, Color(90, 90, 90));
     l.emplace_back("wire_structure_connector", 13, 16, Color(110, 150, 160));
     l.emplace_back("bridge_deck", 14, 17, Color(190, 190, 190));
